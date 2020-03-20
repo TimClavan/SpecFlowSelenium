789f08fb5f30c76f03c13b886fbe00a91a555822
Tim Clavan <ClavanT@wgwa.local>
2020-03-20 11:38:34 +0100

    Add project files.

---
+++ Blazor/Steps/BlazorSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Blazor.Steps
+{
+    [Binding]
+    public class BlazorSteps
+    {
+        [Given(@"I navigate to the home page")]
+        public void GivenINavigateToTheHomePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the homepage is loaded")]
+        public void WhenTheHomepageIsLoaded()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the text ""(.*)"" is present")]
+        public void ThenTheTextIsPresent(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
